91f0193759c5279ef18660c3f6f02d2ad766f339
samsondigitaldesign <124357890+samsondigitaldesign@users.noreply.github.com>
2023-12-30 11:42:27 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['2349155610431'] //ur owner number
+global.ownernomer = "2349155610431" //ur owner number2
+global.ownername = "MRSAM" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub: samsondigitaldesign" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "Cheems Bot MD V8 SamV"
+global.ownernumber = '2349155610431'
+global.ownername = 'MRSAM'
+global.ownerNumber = ["9155610431@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
